04bb6e93b6929c38a279caa7cc13e8575c9e12c1
Mahe9041 <mahesh.official368@gmail.com>
2023-08-08 15:52:04 +0530

    feat : the man look.

--- acme/src/component/Main.js
+++ acme/src/component/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 import { useEffect } from "react";
 import lottie from "lottie-web";
 import reactLogo from "../component/static/animation_lkznporc.json";
@@ -6,7 +7,7 @@ import reactLogo from "../component/static/animation_lkznporc.json";
 export default function Main() {
   useEffect(() => {
     const anime = lottie.loadAnimation({
-      container: document.querySelector("#react-logo"),
+      container: document.querySelector("#Rocket"),
       animationData: reactLogo,
       renderer: "svg", // "canvas", "html"
       loop: true, // boolean
@@ -18,18 +19,35 @@ export default function Main() {
   }, []);
 
   return (
-    <div>
+    <div className="mainBlock">
       <nav>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>
+          Home
+            {/* <Link to="/">Home</Link> */}
+          </li>
+          <li>Home
+            {/* <Link to="/">Clubs</Link> */}
+          </li>
+          <li>Home
+            {/* <Link to="/">Contact Us</Link> */}
+          </li>
+          <li>Home
+            {/* <Link to="/">Feedback</Link> */}
+          </li>
+          <li>Home
+            {/* <Link to="/">Galary</Link> */}
+          </li>
         </ul>
       </nav>
-      <div id="react-logo" />
+      <div className="Main_look">
+        <div className="Main_look_content">
+          <h1 className="m">ACME</h1>
+          <h3 className="m">Science and Cultural Society for ECE department.</h3>
+          <p className="m">A one stop place for all ACME related activites........</p>
+        </div>
+        <div id="Rocket" />
+      </div>
     </div>
   );
 }
